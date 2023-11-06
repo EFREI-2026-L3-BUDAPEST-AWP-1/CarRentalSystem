a0a9c957f046bc959c64733312cce224810aa487
Romain <hello@romain.fr.eu.org>
2023-11-06 19:38:26 +0100

    Missing semicolons

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use('/', express.static('frontend/html'))
-app.use('/img', express.static('frontend/img'))
+app.use('/', express.static('frontend/html'));
+app.use('/img', express.static('frontend/img'));
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`)
+    console.log(`Example app listening on port ${port}!`);
 });
