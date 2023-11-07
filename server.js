c3dd46fe717aa939184ef8947a6e64d7326afbaf
Elegary <21334807+Elegary@users.noreply.github.com>
2023-11-07 18:42:32 +0100

    feat: ejs

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", "views");
+
 const port = process.env.PORT || 3000;
 
-app.use('/', express.static('frontend/html'));
-app.use('/img', express.static('frontend/img'));
+app.get('/', (req, res) => {
+    res.render("index")
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
