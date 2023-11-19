ac9847373bec7c53b7abce360a1da6d71f0815cb
Elegary <21334807+Elegary@users.noreply.github.com>
2023-11-19 20:55:23 +0100

    Added car controller

--- controllers/cars.route.js
+++ controllers/cars.route.js
@@ -2,16 +2,24 @@ const express = require('express');
 
 const router = express.Router();
 
+const carRepository = require('../utils/car.repostitory');
+
 router.get('/list', listAllCars)
+router.get('/:carId', showCarById)
 
 
 async function listAllCars(req, res){
-    const cars = [
-        {car_brand: 'Audi', car_model: 'A4', car_year: 2},
-        {car_brand: 'Audi', car_model: 'A5', car_year: 2},
-        {car_brand: 'Audi', car_model: 'A6', car_year: 2},
-    ]
+    const cars = await carRepository.getAllCars();
+    console.log(cars);
     res.render("index", {cars: cars});
+    //res.send("Hello")
+}
+
+async function showCarById(req, res){
+    const id = req.params.carId;
+    const car = await carRepository.getCarById(id);
+    console.log(car);
+    res.render("cars/show", {car: car[0]});
 }
 
 module.exports = router;
