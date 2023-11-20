9c1a398fbaaef79223a92483fc7640be53399db9
Elegary <21334807+Elegary@users.noreply.github.com>
2023-11-20 19:24:58 +0100

    feat: rent details + changed rent list view

--- controllers/rents.route.js
+++ controllers/rents.route.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const rentsRepository = require('../utils/rents.repository');
 
 router.get('/list', listAllRents)
+router.get('/:rentId', showRentById)
 
 async function listAllRents(req, res){
     const rents = await rentsRepository.getAllRents();
@@ -12,5 +13,11 @@ async function listAllRents(req, res){
     res.render("rents/list", {rents});
 }
 
+async function showRentById(req, res){
+    const id = req.params.rentId;
+    const rent = await rentsRepository.getRentById(id);
+    console.log(rent);
+    res.render("rents/show", {rent: rent[0]});
+}
 
 module.exports = router;
