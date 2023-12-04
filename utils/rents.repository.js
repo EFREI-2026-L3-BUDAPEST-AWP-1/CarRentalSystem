2e3bb4dee2719e6c813276c0330d41ad4bce0f0d
Elegary <21334807+Elegary@users.noreply.github.com>
2023-12-04 17:53:43 +0100

    BIG Features: Rent a car, edit cars, add cars, delete car, finish a rent etc

--- utils/rents.repository.js
+++ utils/rents.repository.js
@@ -8,10 +8,34 @@ module.exports = {
         connection.release();
         return rows;
     },
+    async getAllRentsWithCarAndProfileInfos(){
+        // use JOINS to get the profile information of the renter, the car information and the rent information
+        let connection = await pool.getConnection();
+        const sqlQuery = 'SELECT * FROM Rent JOIN Profile ON Rent.profileID = Profile.profileID JOIN Car ON Rent.carID = Car.carID';
+        let [rows] = await connection.execute(sqlQuery);
+        connection.release();
+        return rows;
+    },
     async getRentById(id){
         let connection = await pool.getConnection();
         const sqlQuery = 'SELECT * FROM Rent WHERE rentID =?';
         let [rows] = await connection.execute(sqlQuery, [id]);
+        connection.release();
+        return rows;
+    },
+    async getRentByIdWithCarAndProfileInfos(id){
+        // use JOINS to get the profile information of the renter, the car information and the rent information
+        let connection = await pool.getConnection();
+        const sqlQuery = 'SELECT * FROM Rent JOIN Profile ON Rent.profileID = Profile.profileID JOIN Car ON Rent.carID = Car.carID WHERE Rent.rentID =?';
+        let [rows] = await connection.execute(sqlQuery, [id]);
+        connection.release();
+        return rows;
+    },
+    async getRentsByCarIdWithProfileInfos(carId){
+        // use JOINS to get the profile information of the renter, the car information and the rent information
+        let connection = await pool.getConnection();
+        const sqlQuery = 'SELECT * FROM Rent JOIN Profile ON Rent.profileID = Profile.profileID JOIN Car ON Rent.carID = Car.carID WHERE Rent.carID =?';
+        let [rows] = await connection.execute(sqlQuery, [carId]);
         connection.release();
         return rows;
     },
@@ -22,4 +46,21 @@ module.exports = {
         connection.release();
         return rows;
     },
+
+    async createRentForCar(carId, profileId, infos){
+        const { tookDate, dueDate } = infos;
+        const price = infos.totalPrice;
+        let connection = await pool.getConnection();
+        const sqlQuery = 'INSERT INTO Rent (carID, profileID, tookDate, dueDate, price) VALUES (?, ?, ?, ?, ?)';
+        let [rows] = await connection.execute(sqlQuery, [carId, profileId, tookDate, dueDate, price]);
+        return rows;
+    },
+
+    async returnCar(rentId, returnedDate){
+        let connection = await pool.getConnection();
+        const sqlQuery = 'UPDATE Rent SET returnedDate = ? WHERE rentID =?';
+        let [rows] = await connection.execute(sqlQuery, [returnedDate, rentId]);
+        connection.release();
+        return rows;
+    }
 }
