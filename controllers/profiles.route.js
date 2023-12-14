f7a7c481bc0d7da3e78ec7ac9367fc68d078788c
Malix-off <alix.brunet@efrei.net>
2023-12-15 00:50:15 +0100

    general profile controller overhaul enhancement

--- controllers/profiles.route.js
+++ controllers/profiles.route.js
@@ -9,19 +9,26 @@ router.get('/list', listAllProfiles);
 router.get('/:profileId', showProfileById);
 
 async function listAllProfiles(req, res){
-    const profiles = await profilesRepository.getAllProfiles();
-    console.log(profiles);
-    res.render("profiles/list", {profiles});
+    try {
+        const profiles = await profilesRepository.getAllProfiles();
+        console.log(profiles);
+        res.render("profiles/list", { profiles });
+    } catch (error) {
+        res.status(500).send("Internal Server Error");
+    }
 }
 
 async function showProfileById(req, res){
-    const id = req.params.profileId;
-    const profile = await profilesRepository.getProfileById(id);
-    const rentsOfProfile = await rentsRepository.getRentsByProfileId(id);
-    console.log(profile);
-    console.log(rentsOfProfile);
-    res.render("profiles/show", {profile: profile[0], rentsOfProfile});
+    try {
+        const id = req.params.profileId;
+        const profile = await profilesRepository.getProfileById(id);
+        const rentsOfProfile = await rentsRepository.getRentsByProfileId(id);
+        console.log(profile);
+        console.log(rentsOfProfile);
+        res.render("profiles/show", { profile: profile[0], rentsOfProfile });
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+    }
 }
-
 
 module.exports = router;
