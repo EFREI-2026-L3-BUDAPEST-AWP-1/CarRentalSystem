cb2a81af0afbdbd0929064ce34484a42dd194414
Malix-off <alix.brunet@efrei.net>
2023-12-15 00:48:47 +0100

    general cars controller overhaul enhancement

--- controllers/cars.route.js
+++ controllers/cars.route.js
@@ -1,26 +1,21 @@
 const express = require('express');
-
 const router = express.Router();
-
 const carRepository = require('../utils/car.repostitory');
 const rentsRepository = require('../utils/rents.repository');
-
 const { adminRightsCheck } = require('../utils/middlewares');
-
-// Manage car images upload
-const multer  = require('multer')
+const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, process.cwd() + '/static/img/')
+        cb(null, process.cwd() + '/static/img/');
     },
     filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-      cb(null, file.fieldname + '-' + uniqueSuffix + '-' + file.originalname);
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, file.fieldname + '-' + uniqueSuffix + '-' + file.originalname);
     }
-  })
-  
-const upload = multer({ storage: storage})
+});
+
+const upload = multer({ storage: storage });
 
 router.get('/list', listAllCars);
 router.get('/manage', adminRightsCheck, showCarManagement);
@@ -29,54 +24,77 @@ router.get('/view/:carId', showCarById);
 router.post('/edit/:carId', upload.single('image'), editCarById);
 router.post('/delete/:carId', deleteCarById);
 
-
-async function listAllCars(req, res){
-    const cars = await carRepository.getAllCars();
-    res.render("cars/list", {cars: cars});
-}
-
-async function showCarById(req, res){
-    const id = req.params.carId;
-    const car = await carRepository.getCarById(id);
-    const rents = await rentsRepository.getRentsByCarIdWithProfileInfos(id);
-    console.log(car);
-    res.render("cars/show", {car: car[0], rents: rents});
-}
-
-async function showCarManagement(req, res){
-    const cars = await carRepository.getAllCars();
-    res.render("cars/manage", {cars: cars});
-}
-
-async function createCar(req, res){
-    console.log(req.body);
-    // TODO : check if all fields are filled
-    if(req.file){
-        console.log(req.file);
-        req.body.image = `/static/img/${req.file.filename}`;
-    } else {
-        req.body.image = "";
+async function listAllCars(req, res) {
+    try {
+        const cars = await carRepository.getAllCars();
+        res.render('cars/list', { cars: cars });
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
     }
-    carRepository.createCar(req.body);
-    res.redirect("/cars/manage");
 }
 
-async function editCarById(req, res){
-    const id = req.params.carId;
-    console.log(req.body);
-    // TODO : check if all fields are filled
-    if(req.file){
-        console.log(req.file);
-        req.body.image = `/static/img/${req.file.filename}`;
+async function showCarById(req, res) {
+    try {
+        const id = req.params.carId;
+        const car = await carRepository.getCarById(id);
+        const rents = await rentsRepository.getRentsByCarIdWithProfileInfos(id);
+        console.log(car);
+        res.render('cars/show', { car: car[0], rents: rents });
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
     }
-    carRepository.editCar(id, req.body);
-    res.redirect("/cars/manage");
 }
 
-async function deleteCarById(req, res){
-    const id = req.params.carId;
-    carRepository.removeCar(id);
-    res.redirect("/cars/manage");
+async function showCarManagement(req, res) {
+    try {
+        const cars = await carRepository.getAllCars();
+        res.render('cars/manage', { cars: cars });
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+async function createCar(req, res) {
+    try {
+        console.log(req.body);
+        // TODO: check if all fields are filled
+        if (req.file) {
+            console.log(req.file);
+            req.body.image = `/static/img/${req.file.filename}`;
+        } else {
+            req.body.image = '';
+        }
+        await carRepository.createCar(req.body);
+        res.redirect('/cars/manage');
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+async function editCarById(req, res) {
+    try {
+        const id = req.params.carId;
+        console.log(req.body);
+        // TODO: check if all fields are filled
+        if (req.file) {
+            console.log(req.file);
+            req.body.image = `/static/img/${req.file.filename}`;
+        }
+        await carRepository.editCar(id, req.body);
+        res.redirect('/cars/manage');
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+async function deleteCarById(req, res) {
+    try {
+        const id = req.params.carId;
+        await carRepository.removeCar(id);
+        res.redirect('/cars/manage');
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+    }
 }
 
 module.exports = router;
